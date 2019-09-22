12720b2428d77dbc04f7d4019764b40878be5eba
Luca <dima.91@live.it>
2019-09-22 16:36:28 +0200

    Updated (and finished) Plex media server

--- Docker/Plex/runner.sh
+++ Docker/Plex/runner.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
 cd "$(dirname "$0")"
 
-docker-compose up
+CONFIG_DIR=$HOME/Documents/pi-server/Docker/Plex/config
+DATA_DIR=$HOME/Documents/pi-server/Docker/Plex/data/drive-link
+
+docker run \
+	--name plex \
+	--net host \
+	-p 32400:32400 \
+	--rm \
+	--volume $CONFIG_DIR:/config \
+	--volume $DATA_DIR:/data \
+	greensheep/plex-server-docker-rpi:latest
