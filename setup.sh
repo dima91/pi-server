de8b172005724a9b5bb158fb584a1b326b496a9b
Luca <luca.dimauro@cnit.it>
2020-12-24 12:21:18 +0100

    Added prints and .bash_aliases file installation

--- setup.sh
+++ setup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-# TODO Checking arguments
+# Copying bash_aliases file that contains some alias and functions
+cp ./bash_aliases ~/.bash_aliases
 
 # Instaling apt dependencies
+echo "Installing apt dependencies"
 sudo apt install openvpn ntfs-3g supervisor eject vim build-essential git perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime \
 					libio-pty-perl apt-show-versions python
 
 
 # Installing Docker
+echo "Installing Docker and docker-compose"
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt install apt-transport-https ca-certificates software-properties-common gnupg2
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt install docker.io docker-compose
 sudo usermod -aG docker $USER
+sudo systemctl enable docker
 
 # Installing "Webmin" tool
+echo "Installing Webmin tool"
 sudo bash -c "echo deb https://download.webmin.com/download/repository sarge contrib >>  /etc/apt/sources.list"
 cd ~/Downloads
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
 sudo apt update
 sudo apt install apt-transport-https webmin
-sudo systemctl enable docker
 
 # Adding pi user to www-data group (needed for OwnCloud)
 sudo usermod -aG pi www-data
